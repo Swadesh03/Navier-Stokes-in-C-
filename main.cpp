f6a0f4100fc685a402baf8928af53963cd2254ea
Swadesh Suman <118707050+Swadesh03@users.noreply.github.com>
2024-03-03 16:38:52 -0500

    Pressure_driven_flow

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+/**************************************************************************************
+Author - Swadesh Suman
+Code - Navier Stokes 2D Channel Flow
+Time - forward stepping
+Spatial - backward discretization - first order, Centered discretization - second order
+Location - McGill University, Canada
+**************************************************************************************/
+
+
+#include"ductFlow.h"
+
+int main() {
+
+    ductFlow testCase;
+    testCase.init();
+
+}
